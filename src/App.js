70b5bfdb532908b77a4aed16e0ee8ef8449a1052
humphrey-mutuma <humphreymutuma01@gmail.com>
2021-07-18 22:48:17 +0300

    Add dynamic content from disease

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { MenuItem, FormControl, Select } from "@material-ui/core";
 import "./App.css";
 
 function App() {
   const [countries, setCountries] = useState(["Kenya", "UK", "Spain"]);
 
+  // https://disease.sh/v3/covid-19/countries
+  useEffect(() => {
+    effect;
+    return () => {
+      cleanup;
+    };
+  }, [countries]);
+  
   return (
     <div className="app">
       <div className="app__header">
@@ -12,7 +20,7 @@ function App() {
         <FormControl className="app__dropdown">
           <Select variant="outlined" value="abc">
             {countries.map((country) => (
-              <MenuItem value="Worldwide">{country}</MenuItem>
+              <MenuItem value={country}>{country}</MenuItem>
             ))}
           </Select>
         </FormControl>
