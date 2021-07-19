0e8192447985aa0ca9201d2a4edac1568dc11956
humphrey-mutuma <humphreymutuma01@gmail.com>
2021-07-19 22:48:35 +0300

    Retrieve data using FetchAPI

--- src/App.js
+++ src/App.js
@@ -3,16 +3,24 @@ import { MenuItem, FormControl, Select } from "@material-ui/core";
 import "./App.css";
 
 function App() {
-  const [countries, setCountries] = useState(["Kenya", "UK", "Spain"]);
+  const [countries, setCountries] = useState([]);
 
   // https://disease.sh/v3/covid-19/countries
   useEffect(() => {
-    effect;
-    return () => {
-      cleanup;
+    const getCountriesData = async () => {
+      fetch("https://disease.sh/v3/covid-19/countries")
+        .then((response) => response.json())
+        .then((data) => {
+          const countries = data.map((country) => ({
+            name: country.country,
+            value: country.countryInfo.iso2,
+          }));
+          setCountries(countries);
+        });
     };
+    getCountriesData();
   }, [countries]);
-  
+
   return (
     <div className="app">
       <div className="app__header">
@@ -20,7 +28,7 @@ function App() {
         <FormControl className="app__dropdown">
           <Select variant="outlined" value="abc">
             {countries.map((country) => (
-              <MenuItem value={country}>{country}</MenuItem>
+              <MenuItem value={country.value}>{country.name}</MenuItem>
             ))}
           </Select>
         </FormControl>
