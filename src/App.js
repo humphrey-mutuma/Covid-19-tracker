74684c01c021168ae70a6895877e408e5ca2fcb1
humphrey-mutuma <humphreymutuma01@gmail.com>
2021-07-20 13:04:39 +0300

    Layout complete, with hard-codded data.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from "react";
-import { MenuItem, FormControl, Select } from "@material-ui/core";
+import { MenuItem, FormControl, Select, CardContent } from "@material-ui/core";
 import "./App.css";
 import InfoBox from "./components/InfoBox/InfoBox";
+import Map from "./components/Map/Map";
+import {Card} from "@material-ui/core";
 
 function App() {
   const [countries, setCountries] = useState([]);
@@ -31,22 +33,37 @@ function App() {
 
   return (
     <div className="app">
-      <div className="app__header">
-        <p>Covid-19 TRACKER </p>
-        <FormControl className="app__dropdown">
-          <Select onChange={onCountryChange} variant="outlined" value={country}>
-            <MenuItem value="worldwide">Worldwide</MenuItem>
-            {countries.map((country) => (
-              <MenuItem value={country.value}>{country.name}</MenuItem>
-            ))}
-          </Select>
-        </FormControl>
-      </div>
-      <div className="app__stats">
-        <InfoBox title="corona cases" total={200} cases={123}/>
-        <InfoBox title="Recovered" total={85} cases={845 } />
-        <InfoBox title="Deaths" total={200} cases={123}/>
-      </div>
+      <section className="app__left">
+        <div className="app__header">
+          <p>Covid-19 TRACKER </p>
+          <FormControl className="app__dropdown">
+            <Select
+              onChange={onCountryChange}
+              variant="outlined"
+              value={country}
+            >
+              <MenuItem value="worldwide">Worldwide</MenuItem>
+              {countries.map((country) => (
+                <MenuItem value={country.value}>{country.name}</MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </div>
+        <div className="app__stats">
+          <InfoBox title="corona cases" total={200} cases={123} />
+          <InfoBox title="Recovered" total={85} cases={845} />
+          <InfoBox title="Deaths" total={200} cases={123} />
+        </div>
+        <div className="map">
+          <Map />
+        </div>
+      </section>
+      <Card className="app__right">
+        <CardContent>
+          <h3>Live cases by Country</h3>
+          <h3>Worldwide cases</h3>
+        </CardContent>
+      </Card>
     </div>
   );
 }
--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Map() {
   return (
     <div>
-      
+      <h2>I'm a map</h2>
     </div>
   )
 }
