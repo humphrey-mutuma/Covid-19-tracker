9eb2d0104732e59a868f916de5753f6f2a05e4ef
humphrey-mutuma <humphreymutuma01@gmail.com>
2021-07-20 12:45:08 +0300

    create the cases cards

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { MenuItem, FormControl, Select } from "@material-ui/core";
 import "./App.css";
+import InfoBox from "./components/InfoBox/InfoBox";
 
 function App() {
   const [countries, setCountries] = useState([]);
@@ -13,7 +14,7 @@ function App() {
         .then((response) => response.json())
         .then((data) => {
           const countries = data.map((country) => ({
-            name: country.country,//kenya, United Kingdom
+            name: country.country, //kenya, United Kingdom
             value: country.countryInfo.iso2, // ke, UK
           }));
           setCountries(countries);
@@ -22,18 +23,29 @@ function App() {
     getCountriesData();
   }, [countries]);
 
+  const onCountryChange = (e) => {
+    const countryCode = e.target.value;
+
+    setCountry(countryCode);
+  };
+
   return (
     <div className="app">
       <div className="app__header">
         <p>Covid-19 TRACKER </p>
         <FormControl className="app__dropdown">
-          <Select variant="outlined" value={country}>
+          <Select onChange={onCountryChange} variant="outlined" value={country}>
             <MenuItem value="worldwide">Worldwide</MenuItem>
             {countries.map((country) => (
               <MenuItem value={country.value}>{country.name}</MenuItem>
             ))}
           </Select>
         </FormControl>
+      </div>
+      <div className="app__stats">
+        <InfoBox title="corona cases" total={200} cases={123}/>
+        <InfoBox title="Recovered" total={85} cases={845 } />
+        <InfoBox title="Deaths" total={200} cases={123}/>
       </div>
     </div>
   );
