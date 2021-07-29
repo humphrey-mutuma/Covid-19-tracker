b05dd6b7d6c567d3fb5e5efe0031229796f25d07
humphrey-mutuma <humphreymutuma01@gmail.com>
2021-07-29 20:59:10 +0300

    Fix the dropdown

--- src/App.js
+++ src/App.js
@@ -1,27 +1,29 @@
 import React, { useState, useEffect } from "react";
-import { MenuItem, FormControl, Select, CardContent } from "@material-ui/core";
 import "./App.css";
+import {
+  MenuItem,
+  FormControl,
+  Select,
+  Card,
+  CardContent,
+} from "@material-ui/core";
 import InfoBox from "./components/InfoBox/InfoBox";
-import Map from "./components/Map/Map";
-import { Card } from "@material-ui/core";
-import Table from "./components/Table/Table";
 import LineGraph from "./components/LineGraph/LineGraph";
+import Table from "./components/Table/Table";
 import { sortData, prettyPrintStat } from "./components/utils";
-import "leaflet/dist/leaflet.css";
 import numeral from "numeral";
+import Map from "./components/Map/Map";
+import "leaflet/dist/leaflet.css";
 
-function App() {
-  const [countries, setCountries] = useState([]);
-  const [country, setCountry] = useState("worldwide");
+const App = () => {
+  const [country, setInputCountry] = useState("worldwide");
   const [countryInfo, setCountryInfo] = useState({});
-  const [tableData, setTableData] = useState([]);
-  const [mapCenter, setMapCenter] = useState({
-    lat: 34.80746,
-    lng: -40.4796,
-  });
-  const [mapZoom, setMapZoom] = useState(3);
+  const [countries, setCountries] = useState([]);
   const [mapCountries, setMapCountries] = useState([]);
+  const [tableData, setTableData] = useState([]);
   const [casesType, setCasesType] = useState("cases");
+  const [mapCenter, setMapCenter] = useState({ lat: 34.80746, lng: -40.4796 });
+  const [mapZoom, setMapZoom] = useState(3);
 
   useEffect(() => {
     fetch("https://disease.sh/v3/covid-19/all")
@@ -50,6 +52,8 @@ function App() {
     getCountriesData();
   }, []);
 
+  console.log(casesType);
+
   const onCountryChange = async (e) => {
     const countryCode = e.target.value;
 
@@ -60,7 +64,7 @@ function App() {
     await fetch(url)
       .then((response) => response.json())
       .then((data) => {
-        // setInputCountry(countryCode);
+        setInputCountry(countryCode);
         setCountryInfo(data);
         setMapCenter([data.countryInfo.lat, data.countryInfo.long]);
         setMapZoom(4);
@@ -69,14 +73,14 @@ function App() {
 
   return (
     <div className="app">
-      <section className="app__left">
+      <div className="app__left">
         <div className="app__header">
-          <h1>Covid-19 TRACKER </h1>
+          <h1>COVID-19 Tracker</h1>
           <FormControl className="app__dropdown">
             <Select
-              onChange={onCountryChange}
               variant="outlined"
               value={country}
+              onChange={onCountryChange}
             >
               <MenuItem value="worldwide">Worldwide</MenuItem>
               {countries.map((country) => (
@@ -107,28 +111,28 @@ function App() {
             isRed
             active={casesType === "deaths"}
             cases={prettyPrintStat(countryInfo.todayDeaths)}
+            total={numeral(countryInfo.deaths).format("0.0a")}
           />
         </div>
         <Map
-          casesType={casesType}
           countries={mapCountries}
+          casesType={casesType}
           center={mapCenter}
           zoom={mapZoom}
         />
-      </section>
+      </div>
       <Card className="app__right">
         <CardContent>
-          <h3>Live cases by Country</h3>
-          <Table countries={tableData} />
-          <br /> <br /> <hr />
-          <br />
-          <h3>Worldwide new {casesType}</h3>
-          <br />
-          <LineGraph casesType={casesType} />
+          <div className="app__information">
+            <h3>Live Cases by Country</h3>
+            <Table countries={tableData} />
+            <h3>Worldwide new {casesType}</h3>
+            <LineGraph casesType={casesType} />
+          </div>
         </CardContent>
       </Card>
     </div>
   );
-}
+};
 
 export default App;
